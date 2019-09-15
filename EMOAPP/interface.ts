c722504524ab723dab22f0f8fd755ba316073940
Abdulrahman Alhalabi <abdulrahman.alhalabi@hypatia.tech>
2019-09-15 20:19:05 +0300

    first commit

---
+++ EMOAPP/interface.ts
@@ -0,0 +1,37 @@
+export interface user {
+    name: string
+    timeSpent: string
+friends:string
+finishedPack:   string
+CompletedPack: string
+acrageTime: number
+avrageTimeSpent: number
+}
+
+
+export interface library {
+    Package: "sleep" | 
+}
+
+
+export interface Package {
+content: string
+length: number
+name: string
+
+}
+
+export interface content {
+
+audio: string
+exc: string
+video : string
+
+}
+
+export interface exc {
+question: string
+  answer: string
+    }
+\
+
